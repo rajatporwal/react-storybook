64a8a373111609f3f5e6aa433674bae6f49d2ac3
rajatporwal <rajatporwal1483@gmail.com>
2022-07-13 11:09:40 +0530

    initiated storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+// preview.js is the configuration file for stories we write
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
