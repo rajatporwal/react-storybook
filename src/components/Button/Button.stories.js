111e415d67e6029d6eb4e7e6cc4314f961660088
rajatporwal <rajatporwal1483@gmail.com>
2022-07-15 13:06:02 +0530

    Test deployment

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -15,7 +15,7 @@ export default {
 }
 
 // childrens of Button hierarchy
-export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Primary = () => <Button variant='primary'>Primary Test</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
